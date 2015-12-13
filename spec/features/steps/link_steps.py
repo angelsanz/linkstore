809b374d781437b6e452f7f2175435ec0a42b45d
Ángel Sanz <angelsanzgit@gmail.com>
2015-12-13 00:25:30 +0000

    Refactor features
    - Enhance wording by making the dichotomy between URL and links
    explicit.
    
    - Extract harcoded literals in step definitions to features.
    
    - Fix typo.

--- spec/features/steps/link_steps.py
+++ spec/features/steps/link_steps.py
@@ -1,10 +1,10 @@
 from expects import expect, contain
 
 
-@given(u'an URL and a tag')
-def save_url_and_tag(context):
-    context.an_url = 'https://www.example.com/'
-    context.a_tag = 'favourites'
+@given(u'the URL "{an_url}" and the tag "{a_tag}"')
+def save_url_and_tag(context, an_url, a_tag):
+    context.an_url = an_url
+    context.a_tag = a_tag
 
 @when(u'I request that they be saved')
 def perform_save(context):
@@ -20,19 +20,19 @@ def save_url_and_tags(context, an_url, a_tag, another_tag):
     context.an_url = an_url
     context.given_tags = (a_tag, another_tag)
 
-@when(u'I request that the link be saved with those tags')
+@when(u'I request that the URL be saved with those tags')
 def perform_save_with_both_tags(context):
     context.linkstore.save_link(context.an_url, context.given_tags)
 
-@then(u'they it should be successfully saved')
+@then(u'the URL should be saved with those tags')
 def verify_link_was_saved_with_both_tags(context):
     all_links = context.link_storage.get_all()
     expect(all_links).to(contain((context.an_url, context.given_tags)))
 
 
-@given(u'I have saved a link with tag "{given_tag}"')
-def save_link_with_given_tag(context, given_tag):
-    context.an_url = 'https://www.example.com/'
+@given(u'I have saved the URL "{an_url}" with tag "{given_tag}"')
+def save_link_with_given_tag(context, an_url, given_tag):
+    context.an_url = an_url
     context.given_tag = given_tag
 
     context.linkstore.save_link(context.an_url, context.given_tag)
