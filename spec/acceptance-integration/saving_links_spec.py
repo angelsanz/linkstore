d45d80a2e51771b1d52718aea749c1e2e01ca2f0
Ángel Sanz <angelsanzgit@gmail.com>
2016-02-21 16:38:41 +0100

    saving_links_spec: verifying using the Linkstore API instead of the SqliteLinkStorage API
    Also, remove a bit of duplication

--- spec/acceptance-integration/saving_links_spec.py
+++ spec/acceptance-integration/saving_links_spec.py
@@ -7,29 +7,27 @@ from ..helpers import an_in_memory_sqlite_link_storage_on_any_date, tuple_contai
 
 with description('saving a link'):
     with before.each:
-        self.link_storage = an_in_memory_sqlite_link_storage_on_any_date()
-        self.linkstore = Linkstore(self.link_storage)
+        self.an_url = 'https://www.example.com/'
+        self.linkstore = Linkstore(an_in_memory_sqlite_link_storage_on_any_date())
 
     with context('with one tag'):
         with it('is successfully saved'):
-            an_url = 'https://www.example.com/'
             a_tag = 'favourites'
 
-            self.linkstore.save_link(an_url, a_tag)
+            self.linkstore.save_link(self.an_url, a_tag)
 
-            all_links = self.link_storage.get_all()
+            all_links = self.linkstore.get_all()
             expect(all_links).to(
-                contain(tuple_containing(an_url, (a_tag,)))
+                contain(tuple_containing(self.an_url, (a_tag,)))
             )
 
     with context('with more than one tag'):
         with it('is successfully saved'):
-            an_url = 'https://www.example.com/'
             some_tags = ('favourites', 'misc')
 
-            self.linkstore.save_link(an_url, some_tags)
+            self.linkstore.save_link(self.an_url, some_tags)
 
-            all_links = self.link_storage.get_all()
+            all_links = self.linkstore.get_all()
             expect(all_links).to(
-                contain(tuple_containing(an_url, some_tags))
+                contain(tuple_containing(self.an_url, some_tags))
             )
