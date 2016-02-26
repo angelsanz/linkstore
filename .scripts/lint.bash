7bebb519fb0b9bffaca8e68d28fa5c0a53522173
Ángel Sanz <angelsanzgit@gmail.com>
2016-02-26 21:34:11 +0100

    Fix ignoring mamba names when running flake8
    The behaviour is exactly the same as before: we still run the risk
    of ignoring some of those names in non-spec files, where it could be
    dangerous. That is an unlikely scenario, however, given those names
    are rather specific to spec files (save for `self`, which is itself
    almost exclusive to methods, where it is always defined, and an error
    would likely be raised otherwise).

--- .scripts/lint.bash
+++ .scripts/lint.bash
@@ -2,11 +2,4 @@
 
 set -e
 
-run_flake8_and_ignore_bdd_constants() {
-    flake8_output_ignoring_bdd_constants=$(flake8 | grep --invert-match "F821 undefined name '\(description\|context\|it\|given\|when\|then\|before\|after\|self\)'$" || true)
-    echo "$flake8_output_ignoring_bdd_constants"
-
-    [[ -z "$flake8_output_ignoring_bdd_constants" ]] || exit 1
-}
-
-run_flake8_and_ignore_bdd_constants
+flake8
