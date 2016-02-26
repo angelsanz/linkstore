322a6a94935977b2a1ff7dd96fb43110a819de40
Ángel Sanz <angelsanzgit@gmail.com>
2016-02-26 14:04:02 +0100

    link_store: remove 'a'/'an' prefix in variable names
    I'd rather reserve this "pattern" for tests.

--- linkstore/link_storage.py
+++ linkstore/link_storage.py
@@ -24,9 +24,9 @@ class SqliteLinkStorage(object):
         id_of_newly_created_link = self._links_table.get_id_by_url(link.url)
         self._tags_table.save_tags_for_link_with_id(id_of_newly_created_link, link.tags)
 
-    def find_by_tag(self, a_tag):
+    def find_by_tag(self, tag):
         matching_links = []
-        for link_id in self._tags_table.get_ids_of_links_with_tag(a_tag):
+        for link_id in self._tags_table.get_ids_of_links_with_tag(tag):
             url, date = self._links_table.get_url_and_date_by_id(link_id)
 
             matching_links.append((url, self._tags_table.get_tags_by_id(link_id), date))
@@ -86,28 +86,28 @@ class LinksTable(SqliteTable):
         with self._connection as connection:
             return connection.execute('select link_id, url, date_saved from links').fetchall()
 
-    def get_id_by_url(self, an_url):
+    def get_id_by_url(self, url):
         with self._connection as connection:
             row_of_link_with_given_url = connection.execute(
                 'select link_id from links where url = ?',
-                (an_url,)
+                (url,)
             ).fetchone()
             desired_id = row_of_link_with_given_url[0]
 
             return desired_id
 
-    def save(self, an_url, a_date):
+    def save(self, url, date):
         with self._connection as connection:
             connection.execute(
                 'insert into links(url, date_saved) values(?, ?)',
-                (an_url, a_date)
+                (url, date)
             )
 
-    def get_url_and_date_by_id(self, a_link_id):
+    def get_url_and_date_by_id(self, link_id):
         with self._connection as connection:
             return connection.execute(
                 'select url, date_saved from links where link_id = ?',
-                (a_link_id,)
+                (link_id,)
             ).fetchone()
 
 
@@ -132,20 +132,20 @@ class TagsTable(SqliteTable):
                 [(link_id, tag) for tag in tags]
             )
 
-    def get_ids_of_links_with_tag(self, a_tag):
+    def get_ids_of_links_with_tag(self, tag):
         with self._connection as connection:
             list_of_rows = connection.execute(
                 'select link_id from tags where name = ?',
-                (a_tag,)
+                (tag,)
             ).fetchall()
 
             return (link_id for (link_id,) in list_of_rows)
 
-    def get_tags_by_id(self, a_link_id):
+    def get_tags_by_id(self, link_id):
         with self._connection as connection:
             list_of_rows = connection.execute(
                 'select name from tags where link_id = ?',
-                (a_link_id,)
+                (link_id,)
             ).fetchall()
 
             return tuple(tag for (tag,) in list_of_rows)
