dea7d60256275bc12f5a9fb947c5f46b0446d9ea
Ángel Sanz <angelsanzgit@gmail.com>
2016-02-27 13:32:34 +0100

    link_spec: add missing comma to actually define tuples

--- spec/unit/link_spec.py
+++ spec/unit/link_spec.py
@@ -7,20 +7,20 @@ with description('Link'):
     with context('is a value object'):
         with it('respects equality'):
             expect(
-                Link('an url', ('a tag'), 'a date')
+                Link('an url', ('a tag',), 'a date')
             ).to(equal(
-                Link('an url', ('a tag'), 'a date')
+                Link('an url', ('a tag',), 'a date')
             ))
 
         with it('respects non-equality'):
             pairs_of_different_links = [
-                (Link('an url', ('a tag'), 'a date'), Link('another url', ('a tag'),       'a date')),
-                (Link('an url', ('a tag'), 'a date'), Link('an url',      ('another tag'), 'a date')),
-                (Link('an url', ('a tag'), 'a date'), Link('an url',      ('a tag'),       'another date')),
-                (Link('an url', ('a tag'), 'a date'), Link('another url', ('another tag'), 'a date')),
-                (Link('an url', ('a tag'), 'a date'), Link('another url', ('a tag'),       'another date')),
-                (Link('an url', ('a tag'), 'a date'), Link('an url',      ('another tag'), 'another date')),
-                (Link('an url', ('a tag'), 'a date'), Link('another url', ('another tag'), 'another date'))
+                (Link('an url', ('a tag',), 'a date'), Link('another url', ('a tag',),       'a date')),
+                (Link('an url', ('a tag',), 'a date'), Link('an url',      ('another tag',), 'a date')),
+                (Link('an url', ('a tag',), 'a date'), Link('an url',      ('a tag',),       'another date')),
+                (Link('an url', ('a tag',), 'a date'), Link('another url', ('another tag',), 'a date')),
+                (Link('an url', ('a tag',), 'a date'), Link('another url', ('a tag',),       'another date')),
+                (Link('an url', ('a tag',), 'a date'), Link('an url',      ('another tag',), 'another date')),
+                (Link('an url', ('a tag',), 'a date'), Link('another url', ('another tag',), 'another date'))
             ]
 
             for a_link, a_different_link in pairs_of_different_links:
